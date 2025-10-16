d7668de26cf5181a819730fde8fd4ab083083f90
Venin1084 <ciasnogb1084@gmail.com>
2025-10-16 16:40:45 +0200

    Prova-1
    Signed-off-by: Venin1084 <ciasnogb1084@gmail.com>

---
+++ .svelte-kit/generated/client/nodes/6.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/contatti/+page.svelte";
--- src/lib/components/Prodotti.svelte
+++ src/lib/components/Prodotti.svelte
@@ -5,35 +5,36 @@
 </script>
 
 
-
+<div class="bg-gray-100">
 <SectionWrapper>
-    <div class="lg:gap-15 flex flex-1 items-center justify-center p-2 md:pb-14 lg:px-67 bg-white ">
+    <div class="lg:gap-5 flex items-center justify-center p-2 md:pb-14 lg:px-67 bg-white ">
         <!-- FOTO 1 -->
            <div class="flex justify-center">
             <picture>
-            <img class="w-full" src="/images/heroImg.png" alt="sedia">
+            <img  src="/images/heroImg.png" alt="sedia">
             </picture>
         </div>
         <!-- FOTO 2 -->
 
            <div class="flex justify-center">
             <picture>
-            <img class="w-full" src="/images/heroImg.png" alt="sedia">
+            <img  src="/images/heroImg.png" alt="sedia">
             </picture>
         </div>
         <!-- FOTO 3 -->
     <div class="flex justify-center">
             <picture>
-            <img class="w-full" src="/images/heroImg.png" alt="sedia">
+            <img  src="/images/heroImg.png" alt="sedia">
             </picture>
         </div>
         <!-- FOTO 4 -->
 
             <div class="flex justify-center">
             <picture>
-            <img class="w-full" src="/images/heroImg.png" alt="sedia">
+            <img  src="/images/heroImg.png" alt="sedia">
             </picture>
         </div>
 
     </div>
 </SectionWrapper>
+</div>
