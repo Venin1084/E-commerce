d66a8cf13dec0fabf28a0f07c228ebbccf690551
Venin1084 <ciasnogb1084@gmail.com>
2025-10-16 16:18:36 +0200

    test commit

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -9,7 +9,7 @@
 <SectionWrapper>
     <div class="lg:gap-15 flex flex-1 items-center justify-center p-2 md:pb-14 lg:px-67">
         <div class=" max-w-xl">
-            <h1 class="lg:text-3xl text-2xl font-bold mb-4">Comfortable Sofa with Great Comfort</h1>
+            <h1 class="lg:text-3xl text-2xl font-bold mb-4">Neko</h1>
             <p class="mb-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.</p>
             <CTAs/>
         </div>
