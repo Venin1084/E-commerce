377f4fb4827bbf96f893a62124d8e5c77be0ab56
Venin1084 <ciasnogb1084@gmail.com>
2025-10-16 16:51:00 +0200

    section product
    Signed-off-by: Venin1084 <ciasnogb1084@gmail.com>

--- src/lib/components/Prodotti.svelte
+++ src/lib/components/Prodotti.svelte
@@ -5,33 +5,33 @@
 </script>
 
 
-<div class="bg-gray-100">
+<div class="bg-gray-100 lg:px-65">
 <SectionWrapper>
-    <div class="lg:gap-5 flex items-center justify-center p-2 md:pb-14 lg:px-67 bg-white ">
+    <div class="lg:gap-5 flex flex-1 items-center justify-center p-2 md:pb-14  bg-white ">
         <!-- FOTO 1 -->
            <div class="flex justify-center">
             <picture>
-            <img  src="/images/heroImg.png" alt="sedia">
+            <img class="w-full" src="/images/heroImg.png" alt="sedia">
             </picture>
         </div>
         <!-- FOTO 2 -->
 
            <div class="flex justify-center">
             <picture>
-            <img  src="/images/heroImg.png" alt="sedia">
+            <img class="w-full" src="/images/heroImg.png" alt="sedia">
             </picture>
         </div>
         <!-- FOTO 3 -->
     <div class="flex justify-center">
             <picture>
-            <img  src="/images/heroImg.png" alt="sedia">
+            <img class="w-full" src="/images/heroImg.png" alt="sedia">
             </picture>
         </div>
         <!-- FOTO 4 -->
 
             <div class="flex justify-center">
             <picture>
-            <img  src="/images/heroImg.png" alt="sedia">
+            <img class="w-full" src="/images/heroImg.png" alt="sedia">
             </picture>
         </div>
 
